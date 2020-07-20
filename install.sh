f6da4d8516050fcd230a5947d5287aac1387301c
Michel Boudreau <michelboudreau@gmail.com>
2020-07-21 09:08:12 +1000

    Adding messaging to install script

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ YELLOW=$(tput setaf 3)
 RESET=$(tput sgr0)
 
 DIRECTORY=~/.local/bin
-declare -a SCRIPTS=("create-timelapse" "dev-computer-setup" "get-temporary-token" "install-docker" "install-hashicorp" "install-p4merge-git")
+declare -a SCRIPTS=("create-timelapse.sh" "dev-computer-setup.sh" "get-temporary-token.sh" "install-docker.sh" "install-hashicorp.sh" "install-p4merge-git.sh")
 
 function show_help {
     echo "${GREEN}${BOLD}Install Gist Scripts Locally${RESET}"
@@ -35,17 +35,23 @@ while getopts "d:h:?:" opt; do
 done
 shift $((OPTIND-1))
 
+echo "${YELLOW}Installing scripts to '${DIRECTORY}'${RESET}"
+
 # Creates directory if not available
 mkdir -p $DIRECTORY
 
 # Add scripts to directory
 for scriptName in "${SCRIPTS[@]}"
 do
-  script="$DIRECTORY/$scriptName.sh"
-  echo -e "#!/usr/bin/env bash\nsource <(curl -s https://raw.githubusercontent.com/mboudreau/gists/master/${scriptName}.sh)" > "$script"
+  script="$DIRECTORY/$scriptName"
+  echo -e "#!/usr/bin/env bash\nsource <(curl -s https://raw.githubusercontent.com/mboudreau/gists/master/${scriptName})" > "$script"
   chmod +x "$script"
 done
 
+echo "${GREEN}Successfully installed scripts to '${DIRECTORY}': ${SCRIPTS[*]}${RESET}"
+
 # add directory to bash profile & reload in current terminal
+echo "${YELLOW}Adding scripts to ${HOME}/.profile${RESET}"
 echo -e "# set PATH so it includes user's private bin if it exists\nif [ -d \"${DIRECTORY}\" ] ; then\n    PATH=\"${DIRECTORY}:$PATH\"\nfi" >> ~/.profile
 source ~/.profile
+echo "${GREEN}Done. You can now reference the scripts directly.${RESET}"
